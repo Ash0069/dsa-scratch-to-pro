4794ddc4251cf5179aeaee1bb70c31b23541f944
Nishant Agrawal <nishantagrawal2003@gmail.com>
2026-03-23 14:23:30 +0530

    array basic operations

--- arrays/array.py
+++ arrays/array.py
@@ -32,6 +32,54 @@ class Array:
     def deleteindex(self, n):
         del self.data[n]
 
+    def deletevalue(self, n):
+        self.data.remove(n)
+
+    def elementbyindex(self, n):
+        print(self.data[n])
+
+    def updateelement(self, n, i):
+        self.data[i] = n
+
+    def search(self, x):
+        for i in range(len(self.data)):
+            if self.data[i] == x:
+                print("Element ",x," exists at index", i)
+                return
+        print("Element not found")
+
+    def maxi(self):
+        c = self.data[0]
+        for i in self.data:
+            if i > c:
+                c = i
+        return c
+    
+    def mini(self):
+        c = self.data[0]
+        for i in self.data:
+            if i < c:
+                c = i
+        return c
+    
+    def sums(self):
+        c = 0
+        for i in self.data:
+            c = c + i
+        return c
+    
+    def avg(self):
+        s = self.sums()
+        n = len(self.data)
+        return s/n
+    
+    def count(self, n):
+        c = 0
+        for i in self.data:
+            if i == n:
+                c += 1
+        return c
+
 arr = Array(3)
 print(arr.data)
 arr.arrinsert(1)
@@ -42,7 +90,16 @@ arr.insertatend(4)
 print(arr.data)
 arr.insertatbeginning(5)
 print(arr.data)
-arr.deleteend()
+# arr.deleteend()
+# print(arr.data)
+# arr.deletevalue(2)
+# print(arr.data)
+arr.elementbyindex(0)
+arr.updateelement(5, 2)
 print(arr.data)
-arr.deleteindex(2)
-print(arr.data)
+arr.search(9)
+print(arr.maxi())
+print(arr.mini())
+print(arr.sums())
+print(arr.avg())
+print(arr.count(5))
